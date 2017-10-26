cbeeb8e629730a3680185d98e7e771c18280dc3d
DenisBarzanov <denis.barzanov2002@gmail.com>
2017-10-26 10:51:36 +0300

    Uploaded from github
    Current latest until "Tapoto" edit

--- html/deposit.php
+++ html/deposit.php
@@ -1,54 +1,59 @@
 <?php require 'testsession.php'; ?>
 <?php require 'navbar.php'; ?>
 <?php
-	$app = 730;
-	$url = $steamprofile['profileurl']."/inventory/json/" . $app . "/2/";
-	$res = file_get_contents($url);
-	$data = json_decode($res, true);
-	$items =  $data["rgDescriptions"];
-	$trade= $data["rgInventory"];
-	$keys=array_keys($items);
-	$keys2=array_keys($trade);
+$app = 730;
+$url = $steamprofile['profileurl'] . "/inventory/json/" . $app . "/2/";
+$res = file_get_contents($url);
+$data = json_decode($res, true);
+if(!$data){
+    echo 'too many requests';
+    die();
+}
+$items = $data["rgDescriptions"];
+$trade = $data["rgInventory"];
+$keys = array_keys($items);
+$keys2 = array_keys($trade);
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title></title>
-	</head>
-	<body background="img/background.jpeg">
-		  <div class="container text-center">
-		    <h1><p style="color:white;">Deposit items</p></h1>
-			<form class="" action="deposit.php" method="post">
-				<button type="button" class="btn btn-info">Deposit Selected</button>
-			</form>
-			<br><br>
-		  </div>
-		<div class="flex-container">
-			<?php for($i=0;$i<count($items);$i++){ ?>
-			<?php $item = $items[$keys[$i]];?>
-			<?php $assetid = $trade[$keys2[$i]];?>
-			<?php if($item['tradable'] == 1){?>
-			<div class="flex-item" id="<?php echo $assetid['id']?>" onclick="select(this.id);">
-					<p style='color: white;'><?php echo $item['name'];?></p><br>
-				 <img src='http://cdn.steamcommunity.com/economy/image/<?php echo $item['icon_url'];?>' />
-			</div>
-			<?php }} ?>
-		</div>
-		<script type="text/javascript">
-			function select(id) {
-				var el = document.getElementById(id);
-				if(el.style.backgroundColor !== "grey"){
-					el.style.backgroundColor="grey";
-					el.style.border="none";
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body background="img/background.jpeg">
+<div class="container text-center">
+    <h1 style="color:white;margin: 0 40px">Deposit items</h1>
+    <form class="" action="deposit.php" method="post">
+        <button type="button" class="btn btn-info">Deposit Selected</button>
+    </form>
+    <br><br>
 
-				}
-				else{
-					el.style.border="2px solid white";
-					el.style.backgroundColor="LightGray";
-				}
-			}
-		</script>
-	</body>
+<div class="flex-container">
+    <?php for ($i = 0; $i < count($items); $i++) { ?>
+        <?php $item = $items[$keys[$i]]; ?>
+        <?php $assetid = $trade[$keys2[$i]]; ?>
+        <?php if ($item['tradable'] == 1) { ?>
+            <div class="unselected flex-item" id="<?php echo $assetid['id'] ?>" onclick="select(this.id);">
+                <p class="text"><?php echo $item['name']; ?></p><br>
+                <img src='http://cdn.steamcommunity.com/economy/image/<?php echo $item['icon_url']; ?>' class="img"/>
+            </div>
+        <?php }
+    } ?>
+</div>
+</div>
+<script type="text/javascript">
+    function select(id) {
+        var el = document.getElementById(id);
+
+        if (el.classList.contains('unselected')) {
+            el.classList.add('selected');
+            el.classList.remove('unselected');
+        } else {
+            el.classList.remove('selected');
+            el.classList.add('unselected');
+        }
+    }
+</script>
+</body>
 </html>
--- html/index.php
+++ html/index.php
@@ -1,20 +1,19 @@
 <?php require 'testsession.php'; ?>
 <html>
-	<head>
-	</head>
-	<body background="img/background.jpeg">
-		<?php require 'navbar.php';?>
+<head>
+</head>
+<body background="img/background.jpeg">
+<?php require 'navbar.php'; ?>
 
 
+<div class="container">
+    <h1 style="color:white;">profile info:</h1>
+    <?php foreach ($steamprofile as $key => $value) { ?>
+        <h4 style="color:white;">
+            <?php echo "$key : $value"; ?>
+        </h4>
+    <?php } ?>
+</div>
 
-		<div class="container">
-			<h1 style="color:white;">profile info:</h1>
-			<?php foreach($steamprofile as $key => $value) { ?>
-				<h4 style="color:white;">
-					<?php echo "$key : $value";?>
-				</h4>
-			<?php } ?>
-		</div>
-
-	</body>
+</body>
 </html>
